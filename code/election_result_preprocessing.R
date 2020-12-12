660182697dd017a7017b3863791265fa3c49a509
tkl96 <til41@pitt.edu>
2020-12-12 14:58:57 -0800

    Electorial Votes Added
    Electoral_College: data from Kaggle/Wiki by year.

--- code/election_result_preprocessing.R
+++ code/election_result_preprocessing.R
@@ -1,13 +1,12 @@
 ## Resetting For Economical Data
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-
 # load("../data/Turnout_df.Rda")
 # Turnout_df=Turnout_df[,-c(21,22,26)]
 # save(Turnout_df, file = '../data/Turnout_df.Rda')
-load("../data/Turnout_df.Rda")
-
-
+# load("../data/Turnout_df.Rda")
+# 
+# 
 library(dplyr)
 library(tidyverse)
 library(plyr)
@@ -68,44 +67,47 @@ library(plyr)
 
 ### Economic Influence during Pandemic 
 ## Personal Income 2020Q2
-pi20=read.csv("pi20Q2_sum.csv",skip=4)[1:180,]
-
-pi20_df=data.frame(State = factor(pi20$GeoName %>% unique() %>% str_remove_all("[*]") %>% sub(' $', '',.)), 
-                   PI20Q2_Tot = (pi20 %>% ddply(.(GeoName), summarise, X2020.Q2[LineCode == 1]) %>% .$..1),
-                   PI20Q2_Cap = (pi20 %>% ddply(.(GeoName), summarise, X2020.Q2[LineCode == 3]) %>% .$..1),
-                   pop20Q2= (pi20 %>% ddply(.(GeoName), summarise, X2020.Q2[LineCode == 2]) %>% .$..1))
-pi20_df[,-1]=pi20_df[order(order(pi20_df$State)),-1]
-## Real GDP
-gdp20=read.csv("qgdp.csv",skip = 4)[1:60,]
-gdp20_df =data.frame(State= gdp20$GeoName %>% str_remove_all("[*]") %>% sub(' $', '',.),
-                     GDP20Q2_Tot=gdp20$X2020.Q2,
-                     GDP20Q2_Cap=gdp20$X2020.Q2*10^6/pi20_df$pop20Q2)
-
-### Economy Behavior 2019
-# # rm(list = ls())
-
-## Desposable Personal Income 
-dpi2019 = read.csv("../data/dpi2019_sum.csv", skip = 4)[1:180,]
+# pi20=read.csv("pi20Q2_sum.csv",skip=4)[1:180,]
 # 
-dpi19_df = data.frame(State = factor(dpi2019$GeoName %>% unique() %>% str_remove_all("[*]") %>% sub(' $', '',.)), 
-                     DPI19_Tot = dpi2019 %>% ddply(.(GeoName), summarise, X2019[LineCode == 51]) %>% .$..1,
-                     DPI19_Cap = dpi2019 %>% ddply(.(GeoName), summarise, X2019[LineCode == 53]) %>% .$..1,
-                     pop19_bea=dpi2019 %>% ddply(.(GeoName), summarise, X2019[LineCode == 52]) %>% .$..1)
-dpi19_df[,-1]=dpi19_df[order(order(dpi19_df$State)),-1]
-
-## Real GDP 
-gdp19 = read.csv("../data/ygdp19.csv", skip = 4)[1:60,]
-gdp19_df =data.frame(State= factor(gdp19$GeoName %>% str_remove_all("[*]") %>% sub(' $', '',.)),
-                     GDP19_Tot=gdp19$X2019,
-                     GDP19_Cap=gdp19$X2019*10^6/dpi19_df$pop19_bea)
-# Merging Economy Data
-Turnout_df = Turnout_df %>% left_join(pi20_df)
-Turnout_df = Turnout_df %>% left_join(gdp20_df)
-Turnout_df = Turnout_df %>% left_join(dpi19_df)
-Turnout_df = Turnout_df %>% left_join(gdp19_df)
-save(Turnout_df, file = '../data/Turnout_df.Rda')
-rm(list = ls())
-
+# pi20_df=data.frame(State = factor(pi20$GeoName %>% unique() %>% str_remove_all("[*]") %>% sub(' $', '',.)), 
+#                    PI20Q2_Tot = (pi20 %>% ddply(.(GeoName), summarise, X2020.Q2[LineCode == 1]) %>% .$..1),
+#                    PI20Q2_Cap = (pi20 %>% ddply(.(GeoName), summarise, X2020.Q2[LineCode == 3]) %>% .$..1),
+#                    pop20Q2= (pi20 %>% ddply(.(GeoName), summarise, X2020.Q2[LineCode == 2]) %>% .$..1))
+# pi20_df[,-1]=pi20_df[order(order(pi20_df$State)),-1]
+# ## Real GDP
+# gdp20=read.csv("qgdp.csv",skip = 4)[1:60,]
+# gdp20_df =data.frame(State= gdp20$GeoName %>% str_remove_all("[*]") %>% sub(' $', '',.),
+#                      GDP20Q2_Tot=gdp20$X2020.Q2,
+#                      GDP20Q2_Cap=gdp20$X2020.Q2*10^6/pi20_df$pop20Q2)
+# 
+# ### Economy Behavior 2019
+# # # rm(list = ls())
+# 
+# ## Desposable Personal Income 
+# dpi2019 = read.csv("../data/dpi2019_sum.csv", skip = 4)[1:180,]
+# # 
+# dpi19_df = data.frame(State = factor(dpi2019$GeoName %>% unique() %>% str_remove_all("[*]") %>% sub(' $', '',.)), 
+#                      DPI19_Tot = dpi2019 %>% ddply(.(GeoName), summarise, X2019[LineCode == 51]) %>% .$..1,
+#                      DPI19_Cap = dpi2019 %>% ddply(.(GeoName), summarise, X2019[LineCode == 53]) %>% .$..1,
+#                      pop19_bea=dpi2019 %>% ddply(.(GeoName), summarise, X2019[LineCode == 52]) %>% .$..1)
+# dpi19_df[,-1]=dpi19_df[order(order(dpi19_df$State)),-1]
+# 
+# ## Real GDP 
+# gdp19 = read.csv("../data/ygdp19.csv", skip = 4)[1:60,]
+# gdp19_df =data.frame(State= factor(gdp19$GeoName %>% str_remove_all("[*]") %>% sub(' $', '',.)),
+#                      GDP19_Tot=gdp19$X2019,
+#                      GDP19_Cap=gdp19$X2019*10^6/dpi19_df$pop19_bea)
+# # Merging Economy Data
+# Turnout_df = Turnout_df %>% left_join(pi20_df)
+# Turnout_df = Turnout_df %>% left_join(gdp20_df)
+# Turnout_df = Turnout_df %>% left_join(dpi19_df)
+# Turnout_df = Turnout_df %>% left_join(gdp19_df)
+# save(Turnout_df, file = '../data/Turnout_df.Rda')
+# rm(list = ls())
+#### Adding Electorial votes
 load("../data/Turnout_df.Rda")
-
-
+electorial.raw=read.csv("../data/Electoral_College.csv")
+electorial=electorial.raw[which(electorial.raw$Year==2020),-1]
+Turnout_df=Turnout_df%>%left_join(electorial)
+Turnout_df[10,]$Votes=electorial[which(electorial$State=="D.C."),]$Votes
+save(Turnout_df,file = "../data/Turnout_df.Rda")
